8680ca8a2d756c8cd00b5a40981c2707befa65bf
Дарья Зайцева <m2108448@edu.misis.ru>
2024-03-23 00:31:18 +0300

    My auth page + modification config

--- src/main/java/ru/zayceva/spring/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/zayceva/spring/FirstSecurityApp/config/SecurityConfig.java
@@ -3,19 +3,34 @@ package ru.zayceva.spring.FirstSecurityApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import ru.zayceva.spring.FirstSecurityApp.security.AuthProviderImpl;
 import ru.zayceva.spring.FirstSecurityApp.services.PersonDetailsService;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@EnableMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true)
+public class SecurityConfig{
 
     private final PersonDetailsService personDetailsService;
 
@@ -24,15 +39,81 @@ public class SecurityConfig {
         this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//    @Bean
+//    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+////        manager.createUser(User.withUsername("user")
+////                .password(bCryptPasswordEncoder.encode("userPass"))
+////                .roles("USER")
+////                .build());
+////        manager.createUser(User.withUsername("admin")
+////                .password(bCryptPasswordEncoder.encode("adminPass"))
+////                .roles("USER", "ADMIN")
+////                .build());
+//        return manager;
+//    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("password")
+//                .roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
+
+    @Autowired
+    void registerProvider(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
+
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(personDetailsService);
+//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                    authorizationManagerRequestMatcherRegistry
+                            //.requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                            //.requestMatchers("/admin/**").hasAnyRole("ADMIN")
+                            //.requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                            .requestMatchers("/auth/login", "/error").permitAll()
+                            .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .failureUrl("/auth/login")
+                        .defaultSuccessUrl("/hello"));
+        return http.build();
+    }
+
+//    protected void configure(HttpSecurity http) throws Exception{
+//        // конфигурируем сам spring security
+//        // конфигурируем авторизацию
+//        http.authorizeRequests()
+//                .antMatchers("/auth/login", "/error").permitAll()
+//                .anyRequest().autenticated()
+//                .and
+//                .loginProcessingUrl("/process_login")
+//                .defaultSuccessUrl("/hello", true)
+//                .failureUrl("/auth/login?error");
+//
+//    }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//
+//    }
 
     // как щтфруется пароль
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
     }
+
+    //////////////////////////
 //    private final AuthProviderImpl authProvider;
 //
 //    @Autowired
